c2031b7f6c3642158b3c703194ac806a48b44e9d
mathealgou <mathealgou@gmail.com>
2026-01-18 18:38:42 -0300

    fix: printing help without raw arguments

--- utils/output/help/help.go
+++ utils/output/help/help.go
@@ -9,7 +9,6 @@ import (
 func PrintHelp() {
 	fmt.Printf("Usage: %s [OPTIONS] <command> [arguments]\n\n", os.Args[0])
 	fmt.Println("Options:")
-	fmt.Println(arguments.Arguments)
 
 	for _, arg := range arguments.Arguments {
 		fmt.Printf("  %-20s %s\n", arg.Argument, arg.Description)
